129815c6d03bdec535a0d8374a2b53f38fb34dc5
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 19:05:37 -0300

    feat: get user profile route

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,21 +1,29 @@
 import {
   Body,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
+  Request,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserUseCase } from './useCases/create-user.useCase';
 import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuardProvider } from '../../infra/providers/auth-guard.provider';
+import { ProfileUserUseCase } from './useCases/profile-user.useCase';
 
-@Controller('/api/users')
+@Controller('/api')
 @ApiTags('users')
 export class UserController {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly profileUserUseCase: ProfileUserUseCase,
+  ) {}
 
-  @Post()
+  @Post('/users')
   @ApiCreatedResponse({ type: UserEntity })
   async create(@Body() data: CreateUserDTO) {
     try {
@@ -25,5 +33,11 @@ export class UserController {
     } catch (error) {
       throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
     }
+  }
+
+  @Get('/users/profile')
+  @UseGuards(AuthGuardProvider)
+  async profile(@Request() req) {
+    return this.profileUserUseCase.execute(req.user.id);
   }
 }
