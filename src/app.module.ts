eb9bf251fe874c17fefdae7afea48e72a0384747
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 16:43:20 -0300

    feat: implements LoginModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './infra/prisma.module';
 import { UserModule } from './modules/users/user.module';
+import { LoginModule } from './modules/login/login.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UserModule } from './modules/users/user.module';
     }),
     PrismaModule,
     UserModule,
+    LoginModule,
   ],
   controllers: [],
   providers: [],
