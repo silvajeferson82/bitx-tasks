1692a1e403948e8f15846fa2a861ec39d8d40d21
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 00:31:18 -0300

    feat: config ApiProperty

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,19 +1,24 @@
 import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   username: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsEmail()
+  @ApiProperty()
   @IsNotEmpty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 }
