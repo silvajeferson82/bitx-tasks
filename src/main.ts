c9b81273c4e3102c95dc9b23543d62bceebba33f
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 00:48:20 -0300

    feat: whitelist true

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const config = new DocumentBuilder()
     .setTitle('Tasks API')
