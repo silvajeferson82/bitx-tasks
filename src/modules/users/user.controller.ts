8d3949f358ac36f86ac2958e97c41fb9bd37185c
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 00:32:17 -0300

    feat: cinfig ApiTags and ApiCreateresponse

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -2,12 +2,15 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UserEntity } from './entities/user.entity';
 import { CreateUserUseCase } from './useCases/create-user.useCase';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('/api/users')
+@ApiTags('users')
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
   @Post()
+  @ApiCreatedResponse({ type: UserEntity })
   async create(@Body() data: CreateUserDTO) {
     const user = await this.createUserUseCase.execute(data);
 
