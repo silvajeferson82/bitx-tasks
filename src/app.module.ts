74235b06ff0668cc4879ed617265498c588ff573
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-22 23:56:04 -0300

    feat: import userModule and prismaModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './infra/prisma.module';
+import { UserModule } from './modules/users/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
