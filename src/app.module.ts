4161ca1edf57f709d41ab5b5be6b8b2665e2e548
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 23:30:21 -0300

    feat: implements taskModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './infra/prisma.module';
 import { UserModule } from './modules/users/user.module';
 import { LoginModule } from './modules/login/login.module';
+import { TaskUserModule } from './modules/tasks/task-user.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { LoginModule } from './modules/login/login.module';
     PrismaModule,
     UserModule,
     LoginModule,
+    TaskUserModule,
   ],
   controllers: [],
   providers: [],
