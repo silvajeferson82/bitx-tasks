33dc388d3efa418dd577e641401289993f8af0e4
silvajeferson82 <jefersonsilva.contato@gmail.com>
2023-09-23 23:31:51 -0300

    feat: config ApiTags and ApiOkResponse

--- src/modules/login/login.controller.ts
+++ src/modules/login/login.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SignInUseCase } from './usecases/sign-in.useCase';
 import { SignInDTO } from './dto/sign-in.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('/api/auth')
+@ApiTags('auth')
 export class LoginController {
   constructor(private readonly signInUseCase: SignInUseCase) {}
 
   @Post('/signin')
+  @ApiOkResponse({ description: 'Sign in' })
   async signIn(@Body() signInDTO: SignInDTO) {
     const token = await this.signInUseCase.execute(signInDTO);
     return token;
